a9370c27fc66963b9825ee2d9175c84a1d17dc6c
suchit-7x <gtsuchit7x@gmail.com>
2026-08-25 20:23:33 +0530

    air: Configure virtual ab updates

--- device.mk
+++ device.mk
@@ -12,5 +12,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # SDCard replacement functionality
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+ENABLE_VIRTUAL_AB := true
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    product \
+    vendor \
+    odm \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
+
+# Update engine
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
