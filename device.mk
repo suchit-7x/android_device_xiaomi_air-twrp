eb3a30ef7cd62239814cef16e570168ec4b3d18c
suchit-7x <gtsuchit7x@gmail.com>
2026-08-25 20:48:36 +0530

    air: Build bootctrl, health & mtkplpath hals

--- device.mk
+++ device.mk
@@ -44,3 +44,21 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Build MT-PL-Utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
